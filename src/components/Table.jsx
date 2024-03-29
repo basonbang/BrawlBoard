86e475bc7df714043ac4132d6fb72330c6d27b15
Jason <jasontang20043@gmail.com>
2024-03-29 04:34:31 -0700

    Add search and filter features

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,53 +1,90 @@
 import TableRow from "./TableRow";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Table = ({brawlers, events, brawlerStats, gameMode, handleGameModeChange}) => {
+
+  const [inputtedBrawler, setInputtedBrawler] = useState("");
+
+  const handleChange = (event) => {
+    setInputtedBrawler(`${event.target.value}`);
+  }
+
   
+  let rows;
   // Create the rows containing all the brawler information
-  const rows = brawlers.map((brawler) => {
+  if (inputtedBrawler !== "") {
+    const filteredBrawlers = brawlers.filter((brawler) => brawler.name.includes(inputtedBrawler));
 
-    // Grab the every brawler's stats for the current selected gamemode
-    const statsForGameMode = (gameMode) ? brawlerStats[brawler.name]?.[gameMode] : null;
+    rows = filteredBrawlers.map((brawler) => {
+      const statsForGameMode = (gameMode) ? brawlerStats[brawler.name]?.[gameMode] : null;
 
-    return (
-      <TableRow 
-        key={brawler.id} 
-        brawler={brawler}
-        winRate={statsForGameMode ? statsForGameMode.winRate + "%" : 'N/A'}
-        useRate={statsForGameMode ? statsForGameMode.useRate + "%" : 'N/A'}
-      />
+      return (
+        <TableRow 
+          key={brawler.id}
+          brawler={brawler}
+          winRate={statsForGameMode ? statsForGameMode.winRate + "&" : 'N/A'}
+          useRate={statsForGameMode ? statsForGameMode.useRate + "%" : 'N/A'}
+        />
       )
-  })
+    })
+  } else {
+    rows = brawlers.map((brawler) => {
+  
+      // Grab the every brawler's stats for the current selected gamemode
+      const statsForGameMode = (gameMode) ? brawlerStats[brawler.name]?.[gameMode] : null;
+  
+      return (
+        <TableRow 
+          key={brawler.id} 
+          brawler={brawler}
+          winRate={statsForGameMode ? statsForGameMode.winRate + "%" : 'N/A'}
+          useRate={statsForGameMode ? statsForGameMode.useRate + "%" : 'N/A'}
+        />
+        )
+    })
+  }
+
 
   return (
-    <table className="table">
-      <thead>
-        <tr className="brawler-info">
-          <th>Brawler</th>
-          <th>Rarity</th>
-          <th>Abilities</th>
-          <th>
-            <div className="select-container">
-              <span>Win Rate</span>
-              <select name="gamemodes" onChange={handleGameModeChange}>
-                  <option value="None Selected">
-                    Select a Game Mode
-                  </option>
-                {events.map((event) => (
-                  <option value={event.event} key={event.id}>
-                    {event.event}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </th>
-        </tr>
-      </thead>
+    <div>
+      <div className="filters">
+        <div className="input-container">
+          <span>Pick a Brawler!</span>
+          <input
+            type="text"
+            id="filtered-brawler"
+            placeholder="Type in a Brawler!"
+            name="filteredBrawler"
+            value={inputtedBrawler}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input-container">
+          <span>Pick a Game Mode!</span>
+          <select name="gamemodes" onChange={handleGameModeChange}>
+            <option value="None Selected">Select a Game Mode</option>
+            {events.map((event) => (
+              <option value={event.event} key={event.id}>
+                {event.event}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
 
-      <tbody>
-        {rows}
-      </tbody>
-    </table>
+      <table className="table">
+        <thead>
+          <tr className="brawler-info">
+            <th>Brawler</th>
+            <th>Rarity</th>
+            <th>Abilities</th>
+            <th>Win Rate</th>
+          </tr>
+        </thead>
+
+        <tbody>{rows}</tbody>
+      </table>
+    </div>
   );
 }
  
